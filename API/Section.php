799948e3b25f06898ed22f12f060ca3173ba290d
roennel <ronnyg@soundpainters.ch>
2014-05-05 22:45:17 +0200

    Update Section.php
    Cleanup

--- API/Section.php
+++ API/Section.php
@@ -1,30 +1,30 @@
-<?php
-
-namespace T4G\API;
-
-class Section
-{
-  public $id;
-  
-  protected $endpoints = [];
-  
-  public function addEndpoint(Endpoint $endpoint)
-  {
-    $this->endpoints[$endpoint->id] = $endpoint;
-  }
-  
-  public function getEndpoints()
-  {
-    return $this->endpoints;
-  }
-  
-  public function __call($method, $args)
-  {
-    $method = strToLower(str_replace('get', '', $method));
-    
-    $call = $this->endpoints[$method];
-    $call->args = count($args) > 0 ? $args[0] : [];
-    
-    return $call;
-  }
+<?php
+
+namespace T4G\API;
+
+class Section
+{
+  public $id;
+
+  protected $endpoints = [];
+
+  public function addEndpoint(Endpoint $endpoint)
+  {
+    $this->endpoints[$endpoint->id] = $endpoint;
+  }
+
+  public function getEndpoints()
+  {
+    return $this->endpoints;
+  }
+
+  public function __call($method, $args)
+  {
+    $method = strToLower(str_replace('get', '', $method));
+
+    $call = $this->endpoints[$method];
+    $call->args = count($args) > 0 ? $args[0] : [];
+
+    return $call;
+  }
 }
